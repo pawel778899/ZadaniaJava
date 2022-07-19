66ce1be3a75180924ba544cf803108789d40c8cf
pawel778899 <106928352+pawel778899@users.noreply.github.com>
2022-07-19 08:19:25 +0200

    Update main.java

--- src/Zadanie18/main.java
+++ src/Zadanie18/main.java
@@ -1,2 +1,18 @@
-package Zadanie18;public class main {
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pattern pattern = Pattern.compile("(^|\\s)a+\\s+psik($|\\s)");
+        String input = "dsasadsdas aaaaaaaa psik jhgjhgjh";
+
+        boolean result = pattern.matcher(input).find();
+
+        if (result) {
+            System.out.println("User kichnął");
+        } else {
+            System.out.println("User nie kichnął");
+        }
+    }
 }
