3314ab71e28e8162bc02645c911b79451aa9eef7
pawel778899 <106928352+pawel778899@users.noreply.github.com>
2022-07-19 08:22:21 +0200

    Update main.java

--- src/Zadanie20b/main.java
+++ src/Zadanie20b/main.java
@@ -1,2 +1,23 @@
-package Zadanie20b;public class main {
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Bingo bingo = new Bingo();
+        System.out.println(bingo.getAnswer());
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int givenNumber = scanner.nextInt();
+            if (givenNumber > bingo.getAnswer()) {
+                System.out.println("Za dużo");
+            } else if (givenNumber < bingo.getAnswer()) {
+                System.out.println("Za mało");
+            } else {
+                System.out.println("Bingo!");
+                break;
+            }
+        }
+    }
 }
