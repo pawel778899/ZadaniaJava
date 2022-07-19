992e5d5ea70d35906140e0554bffc8f5cf00d0b3
pawel778899 <106928352+pawel778899@users.noreply.github.com>
2022-07-19 08:19:58 +0200

    Update Author.java

--- src/Zadanie19/Author.java
+++ src/Zadanie19/Author.java
@@ -1,2 +1,18 @@
-package Zadanie19;public class Author {
+public class Author {
+
+    private String surname;
+    private String nationality;
+
+    public Author(String surname, String nationality) {
+        this.surname = surname;
+        this.nationality = nationality;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
 }
