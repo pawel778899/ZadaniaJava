89b5aa1c7af10926e031c9af7fca03b496142cf9
pawel778899 <106928352+pawel778899@users.noreply.github.com>
2022-07-19 08:20:59 +0200

    Update Poem.java

--- src/Zadanie19/Poem.java
+++ src/Zadanie19/Poem.java
@@ -1,2 +1,18 @@
-package Zadanie19;public class Poem {
+public class Poem {
+
+    private Author creator;
+    private int stropheNumbers;
+
+    public Poem(Author creator, int stropheNumbers) {
+        this.creator = creator;
+        this.stropheNumbers = stropheNumbers;
+    }
+
+    public Author getCreator() {
+        return creator;
+    }
+
+    public int getStropheNumbers() {
+        return stropheNumbers;
+    }
 }
